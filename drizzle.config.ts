95fb2b91c8ca6d211240b7ce6e6e65c759db4be9
Mimo <mimoriam420@gmail.com>
2024-05-29 04:17:03 +0500

    Commit #7 - Connect drizzle and perform migrations

---
+++ drizzle.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from "drizzle-kit";
+
+import { config } from "dotenv";
+
+config({ path: ".env.local" });
+
+export default defineConfig({
+  dialect: "postgresql",
+  schema: "./server/schema.ts",
+  out: "./server/migrations",
+  dbCredentials: {
+    url: process.env.POSTGRES_URL!,
+  },
+});
---
+++ server/schema.ts
@@ -0,0 +1,9 @@
+import { pgTable, serial, text } from "drizzle-orm/pg-core";
+
+export const posts = pgTable("posts", {
+  id: serial("id").primaryKey().notNull(),
+  title: text("title").notNull(),
+});
+
+export type InsertPost = typeof posts.$inferInsert;
+export type SelectPost = typeof posts.$inferSelect;
