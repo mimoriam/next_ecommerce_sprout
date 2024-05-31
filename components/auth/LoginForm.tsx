7d7a91be9f50a23f38e4b823f026132803e3587a
Mimo <mimoriam420@gmail.com>
2024-05-31 16:20:52 +0500

    Commit #21 - Login form capturing values now

--- components/auth/LoginForm.tsx
+++ components/auth/LoginForm.tsx
@@ -6,7 +6,19 @@ import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { LoginSchema } from "@/types/LoginSchema";
 import * as z from "zod";
-import { Form } from "@/components/ui/form";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import { cn } from "@/lib/utils";
 
 export const LoginForm = () => {
   const form = useForm({
@@ -17,7 +29,9 @@ export const LoginForm = () => {
     },
   });
 
-  const onSubmit = (values: z.infer<typeof LoginSchema>) => {};
+  const onSubmit = (values: z.infer<typeof LoginSchema>) => {
+    console.log(values);
+  };
 
   return (
     <AuthCard
@@ -28,7 +42,60 @@ export const LoginForm = () => {
     >
       <div>
         <Form {...form}>
-          <form onSubmit={form.handleSubmit(onSubmit)}></form>
+          <form onSubmit={form.handleSubmit(onSubmit)}>
+            <div>
+              <FormField
+                control={form.control}
+                name="email"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Email</FormLabel>
+                    <FormControl>
+                      <Input
+                        {...field}
+                        placeholder="Name@email.com"
+                        type="email"
+                        autoComplete="email"
+                      />
+                    </FormControl>
+                    <FormDescription />
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="password"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Password</FormLabel>
+                    <FormControl>
+                      <Input
+                        {...field}
+                        placeholder="*********"
+                        type="password"
+                        autoComplete="current-password"
+                      />
+                    </FormControl>
+                    <FormDescription />
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <Button size={"sm"} variant={"link"} asChild>
+                <Link href={"/auth/reset"}>Forgot your password</Link>
+              </Button>
+            </div>
+            <Button
+              type="submit"
+              className={cn(
+                "w-full",
+                status === "executing" ? "animate-pulse" : "",
+              )}
+            >
+              {"Login"}
+            </Button>
+          </form>
         </Form>
       </div>
     </AuthCard>
