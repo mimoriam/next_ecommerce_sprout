ca495a77838dfef4e20397e72bfc8ac27a2b1b87
Mimo <mimoriam420@gmail.com>
2024-06-02 04:01:45 +0500

    Commit #33 - Credentials login working

--- auth.config.ts
+++ auth.config.ts
@@ -1,5 +1,11 @@
-import type { NextAuthConfig } from "next-auth";
+import { NextAuthConfig } from "next-auth";
 import GitHub from "@auth/core/providers/github";
+import Credentials from "@auth/core/providers/credentials";
+import { LoginSchema } from "@/types/LoginSchema";
+import { users } from "@/server/schema";
+import { eq } from "drizzle-orm";
+import { db } from "@/server";
+import bcrypt from "bcryptjs";
 
 export default {
   providers: [
@@ -7,6 +13,26 @@ export default {
       clientId: process.env.GITHUB_CLIENT_ID,
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
       allowDangerousEmailAccountLinking: true,
+    }),
+    Credentials({
+      authorize: async (credentials) => {
+        const validatedFields = LoginSchema.safeParse(credentials);
+
+        if (validatedFields.success) {
+          const { email, password } = validatedFields.data;
+
+          const user = await db.query.users.findFirst({
+            where: eq(users.email, email),
+          });
+
+          if (!user || !user.password) return null;
+
+          const passwordMatch = await bcrypt.compare(password, user.password);
+
+          if (passwordMatch) return user;
+        }
+        return null;
+      },
     }),
   ],
   secret: process.env.AUTH_SECRET,
--- server/actions/EmailRegisterAction.ts
+++ server/actions/EmailRegisterAction.ts
@@ -2,7 +2,7 @@ import { createSafeActionClient } from "next-safe-action";
 import { RegisterSchema } from "@/types/RegisterSchema";
 import { eq } from "drizzle-orm";
 import { users } from "@/server/schema";
-import bcrpyt from "bcryptjs";
+import bcrypt from "bcryptjs";
 import { db } from "@/server";
 import { generateEmailVerificationToken } from "@/server/actions/VerificationTokenAction";
 import { sendVerificationEmail } from "@/server/actions/SendVerificationEmailAction";
@@ -12,7 +12,7 @@ const action = createSafeActionClient();
 export const emailRegister = action(
   RegisterSchema,
   async ({ email, name, password }) => {
-    const hashedPassword = await bcrpyt.hash(password, 10);
+    const hashedPassword = await bcrypt.hash(password, 10);
 
     const existingUser = await db.query.users.findFirst({
       where: eq(users.email, email),
@@ -21,8 +21,6 @@ export const emailRegister = action(
     if (existingUser) {
       if (!existingUser.emailVerified) {
         const verificationToken = await generateEmailVerificationToken(email);
-
-        // Send token by email
         await sendVerificationEmail(
           verificationToken[0].email,
           verificationToken[0].token,
@@ -30,19 +28,20 @@ export const emailRegister = action(
 
         return { success: "Email Confirmation resent" };
       }
-
       return { error: "Email already in use" };
     }
 
-    // Logic for when the user is not registered
-    await db.insert(users).values({
-      email,
-      name,
-    });
+    await db
+      .insert(users)
+      .values({
+        email,
+        name,
+        password: hashedPassword,
+      })
+      .returning();
 
     const verificationToken = await generateEmailVerificationToken(email);
 
-    // Send token by email
     await sendVerificationEmail(
       verificationToken[0].email,
       verificationToken[0].token,
